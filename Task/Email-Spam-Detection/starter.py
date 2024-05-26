789dfd409a68f0ee3e2b58dbdba248c759c7f2c2
duressa-feyissa <duresa.feyisa.gose@gmail.com>
2024-05-26 19:32:31 +0300

    :bug: fix accuracy

--- Task/Email-Spam-Detection/starter.py
+++ Task/Email-Spam-Detection/starter.py
@@ -1,7 +1,6 @@
-from collections import defaultdict
-
+import numpy as np
 import pandas as pd
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -13,67 +12,68 @@ def process_email(text):
     text = text.lower()
     return list(set(text.split()))
 
+
 emails["words"] = emails["text"].apply(process_email)
 
-spam = emails[emails['spam'] == 1]
-normal = emails[emails['spam'] == 0]
+total_emails_count = len(emails)
+spam_emails_count = len(emails[emails["spam"] == 1])
+normal_emails_count = len(emails[emails["spam"] == 0])
+prior_spam_prob = spam_emails_count / total_emails_count
+prior_normal_prob = normal_emails_count / total_emails_count
 
-total_emails = len(emails)
-total_spam = len(spam)
-total_normal = len(normal)
+spam_emails = emails[emails["spam"] == 1]
+normal_emails = emails[emails["spam"] == 0]
+spam_words = {}
+normal_words = {}
 
-p_spam = total_spam / total_emails
-p_normal = total_normal / total_emails
+for email in spam_emails["words"]:
+    for word in email:
+        if word in spam_words:
+            spam_words[word] += 1
+        else:
+            spam_words[word] = 1
 
-spam_word_freq = defaultdict(int)
-normal_word_freq = defaultdict(int)
+for email in normal_emails["words"]:
+    for word in email:
+        if word in normal_words:
+            normal_words[word] += 1
+        else:
+            normal_words[word] = 1
 
-for words in spam["words"]:
-    for word in words:
-        spam_word_freq[word] += 1
 
-for words in normal["words"]:
-    for word in words:
-        normal_word_freq[word] += 1
+def calculate_posteriors(word):
+    likelihood_spam = (spam_words.get(word, 0) + 1) / (total_spam_words_count + vocab_size)
+    likelihood_normal = (normal_words.get(word, 0) + 1) / (total_normal_words_count + vocab_size)
+    return likelihood_spam, likelihood_normal
 
-def predict_naive_bayes(word):
-    p_word_given_spam = (spam_word_freq[word] + 1) / (total_spam + len(spam_word_freq))
-    p_word_given_normal = (normal_word_freq[word] + 1) / (total_normal + len(normal_word_freq))
-    p_spam_given_word = (p_word_given_spam * p_spam) / ((p_word_given_spam * p_spam) + (p_word_given_normal * p_normal))
-    return p_spam_given_word
 
-def calculate_posterior(email_text):
-    words = process_email(email_text)
-    spam_prob = 1
-    normal_prob = 1
-    
-    for word in words:
-        spam_prob *= (spam_word_freq[word] + 1) / (total_spam + len(spam_word_freq))
-        normal_prob *= (normal_word_freq[word] + 1) / (total_normal + len(normal_word_freq))
-    
-    spam_prob *= p_spam
-    normal_prob *= p_normal
-    
-    if spam_prob > normal_prob:
+total_spam_words_count = sum(spam_words.values())
+total_normal_words_count = sum(normal_words.values())
+vocab_size = len(set(spam_words.keys()).union(set(normal_words.keys())))
+
+def predict_naive_bayes(email):
+    email_words = process_email(email)
+    spam_score = np.log(prior_spam_prob)
+    normal_score = np.log(prior_normal_prob)
+
+    for word in email_words:
+        likelihood_spam, likelihood_normal = calculate_posteriors(word)
+        spam_score += np.log(likelihood_spam)
+        normal_score += np.log(likelihood_normal)
+
+    if spam_score > normal_score:
         return "spam"
     else:
         return "normal"
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/predict_word', methods=['GET'])
-def predict_word():
-    sample_word = request.args.get('word')
-    probability = predict_naive_bayes(sample_word)
-    return jsonify({"word": sample_word, "probability": probability})
 
 @app.route('/predict_email', methods=['POST'])
 def predict_email():
-    sample_email = request.json.get('email')
-    posterior = calculate_posterior(sample_email)
-    return jsonify({"result": posterior})
+    data = request.json
+    email = data.get("email", "")
+    result = predict_naive_bayes(email)
+    return jsonify({"result": result})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
